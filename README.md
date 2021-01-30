afa7edb3c020185426890caa6a86ff2d7705ad78
Pablo Pérez-Aradros <pperez@sshteam.com>
2021-01-30 15:46:04 +0100

    Added draft version of the readme file and receive config from the environment

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # discord-telegram-synchronizer
 Synchronizer tool for mirror all the messages between a Discord channel and a Telegram channel.
+
+# Python version
+## Prerequisites and instalation
+This tool require:
+* Python 3.X
+* Pip package manager
+
+To install the external libraries use the following command:
+```
+pip install -r requirements.txt
+```
+
+# Docker version
+
+## Prerequisites
+You need have installed docker desktop (for Windows and Mac) or Docker Engine (in linux versions).
+
+## Usage
+TODO: Define an example of the usage method with docker and env variables.
--- bot.py
+++ bot.py
@@ -1,38 +1,24 @@
 import requests
 import discord
+import os
 
-TOKEN = 'Nzk0NTEyODY1NzI5MDUyNzAz.X-75xA.pM9JhcM3lxm1HGiELUJx4lcoA8M'
-GUILD = 'Gaymers'
+
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+DISCORD_GUILD = os.environ['DISCORD_GUILD']
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+TELEGRAM_GROUP = os.environ['TELEGRAM_GROUP']
+
 
 client = discord.Client()
-
-
-def get_answer_simsimi(text):
-    data = {
-        "lang": "es",
-        "utext": text,
-    }
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'x-api-key': 'Yd4qd7up-FtfgKd0CUm336XSy0ohpbso4MjaN.Fc',
-    }
-    r = requests.post('https://wsapi.simsimi.com/190410/talk', headers=headers, json=data)
-    return r.json()['atext']
-
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if not '~' in message.content.lower():
-        response = get_answer_simsimi(message.content)
+    sendto_telegram(message.content)
 
-        with open('data.yml', 'a', encoding="utf-8") as the_file:
-            the_file.write('- - ' + message.content.encode().decode('utf-8', 'ignore') + '\n')
-            the_file.write('  - ' + response.encode().decode('utf-8', 'ignore') + '\n')
-            the_file.flush()
-        await message.channel.send(response)
+       
 
 
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
