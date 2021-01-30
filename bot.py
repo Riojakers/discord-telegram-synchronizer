28fd75cdc7a6e2badf2ad8cedf9091e163ba577b
Fermin Jimenez <ferminjimenez93@gmail.com>
2021-01-30 15:54:49 +0100

    telebot

--- bot.py
+++ bot.py
@@ -1,24 +1,26 @@
-import requests
-import discord
 import os
+import discord
 
+import telebot
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 DISCORD_GUILD = os.environ['DISCORD_GUILD']
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TELEGRAM_GROUP = os.environ['TELEGRAM_GROUP']
 
-
 client = discord.Client()
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    sendto_telegram(message.content)
 
-       
+@bot.message_handler()
+def message_handler(message):
+    pass
 
 
 client.run(DISCORD_TOKEN)
